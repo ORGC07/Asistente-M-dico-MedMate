f5fdfdd6906939b4a4fd219cef6f9eba5c074af9
Omar .R Gutierrez <61659475+ORGC07@users.noreply.github.com>
2023-02-28 14:08:59 -0400

    Add files via upload

--- MedMate/src/app/rpaciente/rpaciente.page.ts
+++ MedMate/src/app/rpaciente/rpaciente.page.ts
@@ -10,6 +10,8 @@ import { AlertController } from '@ionic/angular';
 import { AuthService } from '../services/auth.service';
 import { FirestoreService } from '../services/firestore.service';
 import { Router } from '@angular/router';
+import { paciente } from '../models/interface';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-rpaciente',
@@ -19,6 +21,18 @@ import { Router } from '@angular/router';
 export class RpacientePage implements OnInit {
 
   public formularioregistrop: FormGroup;
+
+  newpaciente: paciente= {
+    uid: "",
+    nombre: "",
+    apellido: "",
+    cedula: "",
+    fnacimiento: "",
+    edad: 0,
+    correo:"",
+    contraseña: "",
+    rol: "Paciente",
+  }
   
 
   constructor(private fb: FormBuilder, private alertController: AlertController, 
@@ -52,21 +66,20 @@ export class RpacientePage implements OnInit {
     }
 
     else{
-      var paciente = {
-      uid: "",
-      nombre: f.nombre,
-      apellido: f.apellido,
-      cedula: f.cedula,
-      fnacimiento: f.fnacimiento,
-      edad: f.edad,
-      correo: f.correo,
-      contraseña: f.contraseña,
-      rol: "Paciente",
-    }
+      
+      this.newpaciente.nombre = f.nombre,
+      this.newpaciente.apellido= f.apellido,
+      this.newpaciente.cedula= f.cedula,
+      this.newpaciente.fnacimiento= f.fnacimiento,
+      this.newpaciente.edad= f.edad,
+      this.newpaciente.correo= f.correo,
+      this.newpaciente.contraseña= f.contraseña,
+      this.newpaciente.rol= "Paciente";
+    
 
-    console.log(paciente)
+    console.log(this.newpaciente)
 
-    const res = await this.auth.registrar(paciente.correo, paciente.contraseña).catch(res => {
+    const res = await this.auth.registrar(this.newpaciente.correo, this.newpaciente.contraseña).catch(res => {
       console.log('error')
     })
 
@@ -74,10 +87,10 @@ export class RpacientePage implements OnInit {
       console.log("Usuario creado");
       const path = 'Pacientes'
       const Id = res.user!.uid;
-      paciente.uid = Id;
-      paciente.contraseña = null;
+      this.newpaciente.uid = Id;
+      this.newpaciente.contraseña = "";
 
-      await this.store.createDoc(paciente,path,Id)
+      await this.store.createDoc(this.newpaciente,path,Id)
 
       const alert2 = await this.alertController.create({
         header: 'Usuario creado',
