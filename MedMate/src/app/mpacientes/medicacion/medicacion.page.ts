dae433539bce0a80b732452e2407a98b21067317
ORGC07 <OMARRAFAELGC@GMAIL.COM>
2023-03-09 18:42:07 -0400

    Update

--- MedMate/src/app/mpacientes/medicacion/medicacion.page.ts
+++ MedMate/src/app/mpacientes/medicacion/medicacion.page.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup,FormControl,
   Validators, ReactiveFormsModule} from '@angular/forms';
 import { Router } from '@angular/router';
-import { DatePicker } from '@ionic-native/date-picker';
 import { AlertController } from '@ionic/angular';
 import { time } from 'console';
 import { TimeInterval } from 'rxjs/internal/operators/timeInterval';
