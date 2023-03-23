6c7bab143fe212d8d9cab4f6ee01e9ead49b150f
Erick Betances <77901113+Abrahan209@users.noreply.github.com>
2023-03-22 20:16:09 -0400

    Directorio

--- MedMate/src/app/mpacientes/directorio/directorio.page.ts
+++ MedMate/src/app/mpacientes/directorio/directorio.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from 'src/app/app.component';
+import { AuthService } from 'src/app/services/auth.service';
+import { FirestoreService } from 'src/app/services/firestore.service';
+import { doctor } from "src/app/models/interface";
+import { Router } from '@angular/router';
 
 @Component({
   selector: "app-directorio",
@@ -7,11 +11,49 @@ import { AppComponent } from 'src/app/app.component';
   styleUrls: ["./directorio.page.scss"],
 })
 export class DirectorioPage implements OnInit {
-  constructor( private comp : AppComponent) {}
+  doctores: any[] = [];
+  uid: any;
+  infod: doctor = {
+    uid: "",
+    nombre: "",
+    apellido: "",
+    cedula: "",
+    especialidad: "",
+    correo: "",
+    contraseña: "",
+    rol: "Doctor",
+  };
 
-  ngOnInit() {
+  constructor(
+    private comp: AppComponent,
+    private auth: AuthService,
+    private router: Router,
+    private store: FirestoreService
+  ) {}
+
+  async ngOnInit() {
     this.comp.menu();
+    const uid = await this.auth.getUid();
+    if (uid) {
+      this.uid = uid;
+      this.doctor();
+    }
   }
 
-  
+  doctor() {
+    const path = "Doctores";
+    this.store.consultar<doctor>(path).subscribe((res) => {
+      if (res) {
+        this.doctores = [];
+        res.forEach((r) => {
+            this.infod = r;
+            this.doctores.push(this.infod), console.log(this.doctores);
+          
+        });
+      }
+    });
+  }
+  acita() {
+    this.router.navigate(["../a-citas"]);
+  }
 }
